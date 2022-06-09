79bf9711e8d7be711a97c967f3425cf853d763e0
jangkh <devjang2021@gmail.com>
2022-06-09 15:10:00 +0900

    Order validatoin 기능 구체화 및 에러 수정

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,18 +1,30 @@
 package tacos;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import lombok.Data;
 
 @Data
 public class Order {
 	
+	@NotBlank(message = "Name is required")
 	private String deliveryName;
 	private String deliveryStreet;
 	private String deliveryCity;
 	private String deliveryState;
 	private String deliveryZip;
 	
+	
+	@CreditCardNumber(message = "이거 니 카드 아니지??")
 	private String ccNumber;
+	
+	
 	private String ccExpiration;
+	
+	@Digits(integer=3, fraction=0, message = "Invalid CVV")
 	private String ccCVV;
 	
 }
--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -2,7 +2,10 @@ package tacos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,5 +20,16 @@ public class OrderController {
 	public String orderForm(Model model) {
 		model.addAttribute("order", new Order());
 		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(@Validated Order order, Errors errors) {
+		if (errors.hasErrors()) {
+			log.info("errors: " + errors);
+			return "orderForm";
+		}
+		
+		log.info("Order submitted: " + order);
+		return "redirect:/";
 	}
 }
