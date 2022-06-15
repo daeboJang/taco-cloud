b012c5a28b48577907b9f1ca4aff646adb5a783a
daeboJang <devjang2021@gmail.com>
2022-06-15 14:06:32 +0900

    JPA 를 사용해 DB 에 저장하도록 컨트롤러 수정

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -1,5 +1,6 @@
 package tacos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -7,29 +8,41 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
 @Controller
 @Slf4j
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+	
+	@Autowired
+	private OrderRepository orderRepo;
 
 	@GetMapping("/current")
-	public String orderForm(Model model) {
-		model.addAttribute("order", new Order());
+	public String orderForm() {
 		return "orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder(@Validated Order order, Errors errors) {
+	public String processOrder(@Validated Order order, Errors errors, SessionStatus sessionStatus) {
 		if (errors.hasErrors()) {
 			log.info("errors: " + errors);
 			return "orderForm";
 		}
 		
 		log.info("Order submitted: " + order);
+		
+		orderRepo.save(order);
+		
+		// 세션을 재설정
+		sessionStatus.setComplete();
+		
 		return "redirect:/";
 	}
 }
--- src/main/java/tacos/web/DesignController.java
+++ src/main/java/tacos/web/DesignController.java
@@ -1,43 +1,62 @@
 package tacos.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;		// Ingredient class 내에 정의된 static enum type
-import tacos.Taco;;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;;
 
 @Controller
-@RequestMapping("/design")
 @Slf4j
+@RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignController {
+	
+	private final IngredientRepository ingredientRepo;
+	private TacoRepository tacoRepo;
+	
+	@Autowired
+	public DesignController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
+	}
+	
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+	
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
+	}
+	
 
 	@GetMapping
 	public String showDesignForm(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				  new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				  new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				  new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				  new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				  new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-				  new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				  new Ingredient("CHED", "Cheddar", Type.CHEESE),
-				  new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				  new Ingredient("SLSA", "Salsa", Type.SAUCE),
-				  new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
+		
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 		
 		Type[] types = Ingredient.Type.values();
 		for(Type type: types) {
@@ -51,7 +70,7 @@ public class DesignController {
 	}
 	
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(@Validated Taco design, Errors errors, @ModelAttribute Order order) {
 		if (errors.hasErrors()) {
 			return "design";
 		}
@@ -59,6 +78,9 @@ public class DesignController {
 		// 이 지점에서 타코 디자인(선택된 식자재 내역)을 저장한다.
 		// 추후에 구현
 		log.info("저장할 taco 디자인 이름 :" + design);
+		
+		Taco saved = tacoRepo.save(design);
+		order.addDesign(saved);
 		
 		return "redirect:/orders/current";
 	}
