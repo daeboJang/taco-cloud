dc7ee63563037e6f64ab83629f4571af4282a00b
user <user@DESKTOP-VA77KP8.mshome.net>
2022-06-15 10:53:40 +0900

    모델 Entity 정의

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,5 +1,11 @@
 package tacos;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 
@@ -8,23 +14,50 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable{
 	
-	@NotBlank(message = "Name is required")
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Date placedAt;
+	
+	@NotBlank(message = "Delivery Name is required")
 	private String deliveryName;
+	
+	@NotBlank(message = "Delivery Street is required")
 	private String deliveryStreet;
+	
+	@NotBlank(message = "Delivery City is required")
 	private String deliveryCity;
+	
 	private String deliveryState;
 	private String deliveryZip;
 	
+	@ManyToMany(targetEntity = Taco.class)
+	private List<Taco> tacos = new ArrayList<>();
 	
-	@CreditCardNumber(message = "이거 니 카드 아니지??")
+	
+	//CreditCardNumber(message = "이거 니 카드 아니지??")
+	@NotBlank(message = "이거 니 카드 아니지??")
 	private String ccNumber;
 	
-	
+	// 정규표현식으로 검사해야 함!!!
 	private String ccExpiration;
 	
 	@Digits(integer=3, fraction=0, message = "Invalid CVV")
 	private String ccCVV;
 	
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
+	
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
+	}
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,19 +1,39 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Date createdAt;
 
 	@NotNull
 	@Size(min=5, message="Name 은 최소 5글자 이상이어야 해!")
 	private String name;
 	
-	@Size(min=1, message = "1개 이상의 재료를 선택해라!")
+	@ManyToMany(targetEntity = Ingredient.class)
+	@Size(min=1, message = "최소 1개 이상의 재료를 선택해라!")
 	private List<String> ingredients;
+	
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
 }
